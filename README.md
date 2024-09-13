55155166e84b53031189c1ebd3dd4ca7b99f88a4
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-09-14 03:24:01 +0530

    converted to pulumi git repo

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # gcp-cloud-run-pulumi-module
 
 Pulumi Module to Deploy "Service on Google Cloud Run" w/ Planton Cloud
+
+```shell
+pulumi stack select --stack <pulumi-org-name>/<pulumi-stack-name>
+```
+
+```shell
+export STACK_INPUT_FILE_PATH=<path-to-stack-input-yaml-file>
+pulumi up
+```
--- pkg/outputs/outputs.go
+++ pkg/outputs/outputs.go
@@ -1,11 +1,5 @@
 package outputs
 
-import (
-	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/gcp/gcpcloudrun"
-	"github.com/pulumi/pulumi/sdk/v3/go/auto"
+const (
+	OutputKey = "value"
 )
-
-func PulumiOutputsToStackOutputsConverter(stackOutput auto.OutputMap,
-	input *gcpcloudrun.GcpCloudRunStackInput) *gcpcloudrun.GcpCloudRunStackOutputs {
-	return &gcpcloudrun.GcpCloudRunStackOutputs{}
-}
